e7a2e135ad902ef5144dd76689245f9d623076d8
Piyush Jaiswal <piyushkumarj48@gmail.com>
2026-03-02 12:44:00 +0530

    Fixed edge test cases

--- DSA/Array/secondLargest.java
+++ DSA/Array/secondLargest.java
@@ -1,6 +1,7 @@
 public class secondLargest {
     public static void main(String[] args){
-        int[] arr = {4,5,3,8,34,56,3,12,22};
+        //int[] arr = {4,5,3,8,56,3,12,22};
+        int[] arr = {1,2,3,4,5,6,5,7,9,8,4};
         int max = 0;
         int secondMax = 0;
 
@@ -8,6 +9,10 @@ public class secondLargest {
             if(arr[i] > max){
                 secondMax = max;
                 max = arr[i];
+            } else {
+                if (arr[i] > secondMax && arr[i] != max) {
+                secondMax = arr[i];
+                }
             }
         }
         System.out.println(secondMax);
