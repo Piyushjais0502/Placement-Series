fb5a67b1df79ad750602cdf23d5edf87ab061904
Piyush Jaiswal <piyushkumarj48@gmail.com>
2026-03-02 19:29:07 +0530

    New Way Found

--- DSA/Strings/checkPalindrome.java
+++ DSA/Strings/checkPalindrome.java
@@ -1,7 +1,7 @@
 public class checkPalindrome {
     public static void main(String[] args){
-        String s = "maam";
-        String r = "";
+        String s = "maama";
+        /*String r = "";
         boolean isPalindrome = true;
 
         for(int i = s.length()-1; i>= 0; i--){
@@ -11,7 +11,16 @@ public class checkPalindrome {
             isPalindrome = true;
         } else {
             isPalindrome = false;
+        }*/
+        boolean isPalindrome = true;
+
+        for(int i = 0; i<s.length()/2; i++){
+            if(s.charAt(i) != s.charAt(s.length()-1-i)){
+                isPalindrome = false;
+                break;
+            }
         }
+
         if(isPalindrome){
             System.out.println("Palindrome found");
         } else {
