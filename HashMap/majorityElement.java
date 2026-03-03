954ecd4f85353cf3ed28bf46e60fe816f3f78d4c
Piyush Jaiswal <piyushkumarj48@gmail.com>
2026-03-03 20:52:24 +0530

    Question 1 done

---
+++ HashMap/majorityElement.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class majorityElement{
+    public static void main(String[] args){
+        int[] arr = {1,3,5,3,6,1,1,1,1};
+        HashMap <Integer, Integer> freq = new HashMap<>();
+
+        for(int i = 0; i<arr.length; i++){
+            freq.put(arr[i], freq.getOrDefault(arr[i],0)+1);
+        }
+        for(Integer key : freq.keySet()){
+            if(freq.get(key) > arr.length/3){
+                System.out.println(key);
+            }
+        }
+    }
+}
